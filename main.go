53769c19c4d3f506f23d4e2f3bf654737718b7c1
Brady Love <love.brady@gmail.com>
2015-10-30 02:35:25 -0700

    Add sqlite3 and gorm

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import ()
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
+	"time"
+)
+
+var db gorm.DB
 
 func main() {
-
+	var err error
+	db, err = gorm.Open("sqlite3", "todos.db")
+	if err != nil {
+		panic(err)
+	}
 }
