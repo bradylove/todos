e752cc75658d7f0e5eafa58af94426433fc9efef
Brady Love <love.brady@gmail.com>
2015-10-30 02:35:47 -0700

    Add Todo and table migration

--- main.go
+++ main.go
@@ -8,10 +8,20 @@ import (
 
 var db gorm.DB
 
+type Todo struct {
+	ID          int       `json:"id"`
+	Status      string    `json:"status"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
 func main() {
 	var err error
 	db, err = gorm.Open("sqlite3", "todos.db")
 	if err != nil {
 		panic(err)
 	}
+
+	db.AutoMigrate(&Todo{})
 }
